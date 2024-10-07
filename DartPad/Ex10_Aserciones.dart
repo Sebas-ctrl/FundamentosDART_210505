a2c70b06d993dd407b9b989adc38fd8ffb0a28fb
Sebastián Márquez <210505@utxicotepec.edu.mx>
2024-10-07 17:38:53 -0600

    Práctica 12
    Agregación del Ejercicio 10: Aserciones

---
+++ DartPad/Ex10_Aserciones.dart
@@ -0,0 +1,31 @@
+void main() {
+
+    final mySquare = Square( side: 10 );
+    print("""El lado del cuadrado es de : ${mySquare._side}
+          su área es de: ${ mySquare.area }""");
+    mySquare.side = -4;
+    print("""El lado del cuadrado es de : ${mySquare._side}
+          su área es de: ${ mySquare.area }""");
+}
+
+class Square {
+    double _side; // side * side
+    Square({ required double side })
+    : assert(side > 0, 'El lado del cuadrado debe ser mayor a 0 '),
+    _side = side;
+
+    double get area {
+        return _side * _side;
+    }
+
+    set side( double value ) {
+        print('setting new value $value');
+        if ( value < 0 ) throw 'Value must be >=0';
+
+        _side = value;
+    }
+
+    double calculateArea() {
+        return _side * _side;
+    }
+}
