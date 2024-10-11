c8b61f75ca6b753544b53a46e6df8b9c36e4e964
Sebas-ctrl <210505@utxicotepec.edu.mx>
2024-10-11 14:03:53 -0600

    Práctica 15
    Agregación del Ejercicio 13: Futures

---
+++ DartPad/Ex13_Futures.dart
@@ -0,0 +1,17 @@
+void main() {
+    print("Inicio del programa");
+    httpGet('https://aldair.com')
+    .then(
+    (value) {
+        print(value);
+    }).catchError((err) {
+        print("Error: $err");
+    });
+    print("Fin del programa");
+}
+Future<String> httpGet(String url) {
+    return Future.delayed(const Duration(seconds: 1), () {
+        throw 'Error en la petición https';
+        //return "Respuesta del servidor";
+    });
+}
